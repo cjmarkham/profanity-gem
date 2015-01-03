45d3589d6842bc758396485f8b161ab24cfb6d8c
Carl Markham <doddsey65@gmail.com>
2015-01-03 03:17:29 +0000

    Update concurrent_letter.rb

--- lib/profanity_filter/concurrent_letter.rb
+++ lib/profanity_filter/concurrent_letter.rb
@@ -1,20 +1,16 @@
 module ProfanityFilter
   class ConcurrentLetter
     def sanitize(string)
-      individual_words = string.split(" ")
 
-      individual_words.each_with_index do |word, index|
+      string.split(" ").each_with_index do |word, index|
         replaced = word.gsub(/(.)\1+/, "\\1")
 
-        if @@words.include? replaced
-          string = string.gsub(word, replaced)
-        end
+        string = string.gsub(word, replaced) if @@words.include? replaced
 
         lengths = @@words.map{ |w| w.length }
 
         if word.length < lengths.min
-          spaced_word = "#{individual_words[index - 1]}#{word}#{individual_words[index + 1]}"
-          spaced_word = spaced_word.gsub(" ", "")
+          spaced_word = "#{individual_words[index - 1]}#{word}#{individual_words[index + 1]}".gsub(" ", "")
           replaced = spaced_word.gsub(/(.)\1+/, "\\")
         end
       end
